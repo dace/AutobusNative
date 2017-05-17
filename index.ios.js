4d907a38c382dc45ec7bfb3b067156009974b6a4
hidace <dacewillmott@gmail.com>
2017-05-17 00:37:48 -0400

    setup geolocation

--- index.ios.js
+++ index.ios.js
@@ -13,41 +13,37 @@ import {
 } from 'react-native';
 
 export default class AutobusNative extends Component {
+  getLocation() {
+    return navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const initialPosition = position.coords;
+        const { longitude } = initialPosition;
+        const { latitude } = initialPosition;
+        const currentPosition = {
+          latitude,
+          longitude,
+        };
+        console.log(currentPosition);
+        return currentPosition;
+      },
+      (error) => JSON.stringify(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+      }
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+      <View>
+        <Text>
+          {this.getLocation()}
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('AutobusNative', () => AutobusNative);
