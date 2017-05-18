61b3842c5b38d84f604471fe36559a63f85a4a8f
Dace Willmott <willmott@brooklynunited.com>
2017-05-18 17:36:48 -0400

    add fetch stop action creator

--- src/containers/RoutesList/index.js
+++ src/containers/RoutesList/index.js
@@ -6,6 +6,7 @@ import {
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import fetchCoordinates from './../../actions/fetch-coordinates';
+import fetchStops from './../../actions/fetch-stops';
 
 class RoutesList extends Component {
   componentWillMount() {
@@ -40,21 +41,25 @@ class RoutesList extends Component {
         <Text>This is where the list of routes will render.</Text>
         <Text>This is where the list of routes will render.</Text>
         <Text>This is where the list of routes will render.</Text>
-        {this.printCoords()}        
+        {this.printCoords()}
+        {this.props.fetchStops()}
       </View>
     )
   }
 };
 
 const mapStateToProps = state => {
+  console.log(state);
   return {
     currentPosition: state.currentPosition,
+    nearbyStops: state.nearbyStops,
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchCoordinates: bindActionCreators(fetchCoordinates, dispatch),
+    fetchStops: bindActionCreators(fetchStops, dispatch),
   }
 }
 
