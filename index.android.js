a73810a0a577da8962ac5d1039ad9d7798ef65c9
Dace Willmott <willmott@brooklynunited.com>
2017-05-18 11:57:46 -0400

    update paths

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './containers/App';
+import App from './src/containers/App';
 
 AppRegistry.registerComponent('AutobusNative', () => App);
