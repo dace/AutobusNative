22f21ac8a8070ad11d0b2aeaef62ad1c36aa6046
Dace Willmott <willmott@brooklynunited.com>
2017-05-18 10:27:19 -0400

    add coordinates reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
+import coordinates from './coordinates';
 import nearbyRoutes from './nearby-routes';
 import selectedRoute from './selected-route';
 import nearbyStops from './nearby-stops';
 import selectedStop from './selected-stop';
 
 const rootReducer = combineReducers({
-  nearbyRoutes,
-  selectedRoute,
-  nearbyStops,
-  selectedStop,
+  coordinates,
+  // nearbyRoutes,
+  // selectedRoute,
+  // nearbyStops,
+  // selectedStop,
 });
 
 export default rootReducer;
