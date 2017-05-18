7af1b8608afb4f125a797e0a4158d45d8ee43b8e
Dace Willmott <willmott@brooklynunited.com>
2017-05-18 10:32:29 -0400

    add config for basic screen to display

---
+++ src/containers/RouteList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const RouteList = () => {
+  return (
+    <View>
+      <Text>This is where the list of routes will render.</Text>
+    </View>
+  )
+};
+
+export default RouteList;
