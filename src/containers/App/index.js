777a5447de2508a4cf8a2d0e1375e1f952f643fc
Dace Willmott <willmott@brooklynunited.com>
2017-05-18 10:32:44 -0400

    add config

---
+++ src/containers/App/index.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  createStore,
+  applyMiddleware,
+} from 'redux';
+import {
+  Router,
+  Scene,
+} from 'react-native-router-flux';
+import RouteList from './../../containers/RouteList';
+import reducers from './../../reducers';
+
+const store = createStore(reducers, devToolsEnhancer(), applyMiddleware(thunk));
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Scene key="root">
+            <Scene key="routeList" component={RouteList} title="Routes" initial={true} />
+          </Scene>
+        </Router>
+      </Provider>
+    );
+  }
+}
