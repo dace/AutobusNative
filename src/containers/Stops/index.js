d5af3e9e90f601b0f048e78a39e09da3f0f3ce72
Dace Willmott <willmott@brooklynunited.com>
2017-05-20 02:37:05 -0400

    add actions and fetch logic

---
+++ src/containers/Stops/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableHighlight,
+} from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import fetchCoordinates from './../../actions/fetch-coordinates';
+import fetchStops from './../../actions/fetch-stops';
+import selectStop from './../../actions/select-stop';
+import fetchBuses from './../../actions/fetch-buses';
+
+class Stops extends Component {
+  componentWillMount() {
+    this.props.fetchCoordinates();
+  }
+
+  handleSelectStop(code) {
+    this.props.selectStop(code);
+    this.props.fetchBuses(code)
+  }
+
+  renderStops() {
+    if (!this.props.stopsList) {
+        return (
+        <View>
+          <Text>'LOADING...'</Text>
+        </View>
+      )
+    } else {
+      const allStops = this.props.stopsList.nearbyStops.map(stop => {
+        return (
+          <TouchableHighlight key={stop.stopCode} onPress={() => this.handleSelectStop(stop.stopCode)}>
+            <Text style={styles.listItem}>
+              {stop.crossStreets} - {stop.direction}
+            </Text>
+          </TouchableHighlight>
+        )
+      })
+      return allStops;
+    }
+  }
+  render() {
+    return (
+      <ScrollView style={styles.listWrapper}>
+        {this.props.fetchStops()}
+        {this.renderStops()}
+      </ScrollView>
+    )
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    currentPosition: state.currentPosition,
+    stopsList: state.stops,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCoordinates: bindActionCreators(fetchCoordinates, dispatch),
+    fetchStops: bindActionCreators(fetchStops, dispatch),
+    selectStop: bindActionCreators(selectStop, dispatch),
+    fetchBuses: bindActionCreators(fetchBuses, dispatch),
+  }
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 30,
+  },
+  listWrapper: {
+    marginTop: 60,
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Stops);
